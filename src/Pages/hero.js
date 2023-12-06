e73c7f539790c6098058830ce76916c1727b0ec1
Rediet <redietkfle@gmail.com>
2023-12-06 13:15:14 +0300

    Render hero component

---
+++ src/Pages/hero.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import NavBar from '../Components/NavBar/Navbar';
+import Hero from '../Components/Hero/Hero';
+import Footer from '../Components/Footer/Footer';
+
+const HeroPage = () => {
+
+    return (
+     <>
+       <NavBar />
+       <Hero />
+       <Footer />
+     </>
+  );
+}
+
+export default HeroPage;
