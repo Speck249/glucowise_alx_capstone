6ff1db30bcfd8f760a327b2f8c42ffb660005e4d
Rediet <redietkfle@gmail.com>
2023-12-06 13:13:42 +0300

    Render support component

---
+++ src/Pages/support.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Support from '../Components/Support/Support';
+import Navbar from '../Components/NavBar/Navbar';
+
+
+const SupportPage = () => {
+  return (
+    <>
+      <Navbar />
+      <Support />
+    </>
+  );
+}
+
+export default SupportPage;
