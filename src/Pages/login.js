cec1d0dd3fb4cd257fa1bdacfb9eb7995bb91c53
Rediet <redietkfle@gmail.com>
2023-12-06 13:17:32 +0300

    Render login component

---
+++ src/Pages/login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import LogIn from '../Components/Authentication/Login';
+
+const LoginPage = () => {
+  return (
+    <>
+      <LogIn />
+    </>
+  );
+}
+
+export default LoginPage;
