12101b9d0d21689b44222d20c11cd3526300a3a4
Rediet <redietkfle@gmail.com>
2023-12-08 17:15:34 +0300

    Remove deprecated function call

--- src/index.js
+++ src/index.js
@@ -8,6 +8,6 @@ import App from './App/App.js';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
