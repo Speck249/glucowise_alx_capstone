6f5ba6d68962572bcdb9548ad6cedbbe9a65cc0d
Rediet <redietkfle@gmail.com>
2023-12-09 17:35:20 +0300

    Remove Navigation bar from support page

--- src/Pages/support.js
+++ src/Pages/support.js
@@ -1,12 +1,10 @@
 import React from 'react';
-import Navigation from '../Components/Navigation/Navigation';
 import Support from '../Components/Support/Support';
 
 
 const SupportPage = () => {
   return (
     <>
-      <Navigation />
       <Support />
     </>
   );
