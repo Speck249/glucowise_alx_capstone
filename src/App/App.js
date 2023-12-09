75000223bcfd1ed616f80b5be3bf05c99610215a
Rediet <redietkfle@gmail.com>
2023-12-09 17:36:05 +0300

    Add Support page route to app

--- src/App/App.js
+++ src/App/App.js
@@ -5,6 +5,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeroPage from '../Pages/hero.js';
+import SupportPage from '../Pages/support.js';
 import DashboardPage from '../Pages/dashboard.js';
 import LoginPage from '../Pages/login.js';
 import SignupPage from '../Pages/signup.js';
@@ -14,7 +15,8 @@ const App = () => {
     return (
         <Router>
           <Routes>
-            <Route path='/' element={<HeroPage />} /> 
+            <Route path='/' element={<HeroPage />} />
+            <Route path='/support' element={<SupportPage />} /> 
             <Route path='/dashboard' element={<DashboardPage />} />
             <Route path='/login' element={<LoginPage />} />
             <Route path='/signup' element={<SignupPage />} />
