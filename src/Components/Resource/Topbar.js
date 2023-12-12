8836b3aa345f678ce667ee7acca83b507b72e29a
Rediet <redietkfle@gmail.com>
2023-12-12 14:06:01 +0300

    Create Topbar component as navigation for blog page

---
+++ src/Components/Resource/Topbar.js
@@ -0,0 +1,77 @@
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link as LinkRouter } from 'react-router-dom';
+import { Link as LinkScroll } from 'react-scroll';
+import Scrollspy from 'react-scrollspy';
+
+
+const Topbar = () => {
+  return (
+    <>
+    <Scrollspy
+      items={ ['Home', 'Mindset', 'Diet', 'Lifestyle'] }
+      currentClassName='active'
+      offset={-100}>
+
+      <Navbar expand='lg' className='bg-body-tertiary'>
+        <Container 
+          style={{
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            height: '65px',
+            paddingTop: '2rem',
+            backgroundColor: '#000',
+          }}>
+          <Navbar.Collapse id='basic-navbar-nav' style={{color: '#fff', cursor: 'pointer'}}>
+            <Nav className='me-auto' 
+             style={{
+              display: 'flex',
+             }}>
+
+              <LinkScroll style={{marginRight: '1rem'}}
+                to='/'
+                activeClass='active'
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-100}
+              > Home </LinkScroll>
+              
+              <LinkScroll style={{marginRight: '1rem'}}
+                to='Mindset'
+                activeClass='active'
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-100}
+              > Mindset </LinkScroll>
+              
+              <LinkScroll style={{marginRight: '1rem'}}
+                to='Diet'
+                activeClass='active'
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-100}
+              > Diet </LinkScroll>
+
+              <LinkScroll
+                to='Lifestyle'
+                activeClass='active'
+                spy={true}
+                smooth={true}
+                duration={500}
+                offset={-100}
+              > Lifestyle </LinkScroll>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </Scrollspy>
+    </>
+  );
+}
+
+export default Topbar;
