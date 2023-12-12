bcf1a198e3c9ae56e456313224f6ff97b13bf3c0
Rediet <redietkfle@gmail.com>
2023-12-12 17:20:21 +0300

    Separate component structure from data feeding component

--- src/Components/Resource/Resource.js
+++ src/Components/Resource/Resource.js
@@ -1,117 +1,59 @@
 import React from 'react';
 import { Row, Col, Card, Button } from 'antd';
 import { Link as LinkRouter } from 'react-router-dom';
-import Placeholder from '../../Images/Placeholder.jpg';
-import { FaLinkedin, FaFacebook, FaTwitter, FaQuora, FaYoutube, FaWhatsapp } from 'react-icons/fa';
+import { FaLinkedin, FaFacebook, FaTwitter, FaQuora, FaYoutube, FaWhatsapp, FaArrowUp } from 'react-icons/fa';
+import { mindsetPosts, dietPosts, lifestylePosts } from './Data';
 
 
 const { Meta } = Card;
 const Resource = () => {
-  const blogPosts = [
-    {
-      id: 1,
-      title: 'Schedule Appointments',
-      description: 'View and Manage Your Upcoming Therapy Sessions.',
-      image: Placeholder,
-    },
-
-    {
-      id: 2,
-      title: 'Medication Tracker',
-      description: 'Track and Manage Your Prescribed Medications.',
-      image: Placeholder,
-    },
-    
-    {
-      id: 3,
-      title: 'Mood Journal',
-      description: 'Record and Monitor Your Daily Mood and Emotions.',
-      image: Placeholder,
-    },
-    
-    {
-      id: 4,
-      title: 'Therapist Notes',
-      description: 'Access Notes and Feedback From Your Therapist.',
-      image: Placeholder,
-    },
-        
-    {
-      id: 5,
-      title: 'Payment Service',
-      description: 'Choose Your Payment Plan and Manage Your Fees all.',
-      image: Placeholder,
-    },
-    
-    {
-      id: 6,
-      title: 'Resources Library',
-      description: 'Browse Educational Materials and Helpful Resources.',
-      image: Placeholder,
-    },
-  ];
-
- return (
+  return (
     <div className='blogContainer'
       style={{ 
-        backgroundColor: '#000',
         display: 'flex',
         flexDirection: 'column',
         flexWrap: 'wrap',
-        padding: '3rem'
+        padding: '3rem',
+        backgroundColor: '#000'
     }}>
       
-      {/** Mindset*/}
+      {/** Mindset Blog Posts Section */}
       <section id='Mindset'>
         <div className='blogHeader'
           style={{
             backgroundColor: 'pink',
             display: 'flex', 
             padding: '2rem',
-            marginBottom: '1rem',
+            marginBottom: '2rem',
             border: 'none',
             borderRadius: '2rem'
           }}>
          
-          <h2 
-            style={{
-              fontSize: '3rem',
-              color: '#fff',
-              textAlign: 'center',
-            }}> Mindset ipsum dolor sit amet </h2>
-        
-          <p 
-            style={{
-              fontSize: '0.9rem',
-              fontStyle: 'italic',
-              color: '#fff', 
-              textAlign: 'center',
-              lineHeight: 1.6,
-              width: '55%',
-              marginTop: '1rem' 
-            }}>
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt."<br/>
-          <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '1rem'}}> ~ Mr. Galloway</cite></p>
+          <h2 style={{ fontSize: '3rem', color: '#fff', textAlign: 'center' }}> Mindset ipsum dolor sit amet</h2>
+          <p style={{
+            fontSize: '0.9rem',
+            fontStyle: 'italic',
+            color: '#fff', 
+            textAlign: 'center',
+            lineHeight: 1.5,
+            width: '55%',
+            marginTop: '1rem' 
+          }}> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+            eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt."<br/>
+            <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '1rem'}}> ~ Mr. Galloway</cite></p>
         </div>
       
         <div className='blogContent'
           style={{
-            paddingLeft: '1rem',
-            paddingRight: '1rem',
+            paddingLeft: '2rem',
+            paddingRight: '2rem',
             paddingTop: '1rem'
           }}>
         
-          <Row gutter={25}>
-            {blogPosts.map((post) => (
+          <Row gutter={20}>
+            {mindsetPosts.map((post) => (
               <Col span={8} key={post.id}>
-                <Card 
-                  style={{
-                    backgroundColor: 'pink',
-                    border: 'none', 
-                    marginBottom: '1rem'
-                  }}
-
+                <Card style={{ backgroundColor: 'pink', border: 'none', marginBottom: '1rem' }}
                   hoverable
                   cover={ <img src={post.image} alt={post.title} style={{backgroundRepeat: 'no-repeat'}} />} >
 
@@ -124,40 +66,34 @@ const Resource = () => {
         </div>
       </section>
 
-      {/** Diet*/}
+      {/** Diet Blog Section */}
       <section id='Diet'>
-        <div className='blogHeader'
-          style={{
-            backgroundColor: 'pink',
-            display: 'flex', 
-            padding: '2rem',
-            marginTop: '1rem',
-            border: 'none',
-            borderRadius: '2rem'
-          }}>
+        <div className='dietSection' style={{ display: 'flex' }}>
+          <div className='blogHeader'
+            style={{flex: 1,
+              padding: '1.5rem',
+              marginTop: '2rem',
+              border: 'none',
+              borderRadius: '4rem',
+            }}>
         
-          <h2 
-            style={{
-              fontSize: '3rem',
-              color: '#fff',
-              textAlign: 'center',
-            }}> Diet ipsum dolor sit amet</h2>
-        
-          <p 
-            style={{
+            <p style={{
               fontSize: '0.9rem',
               fontStyle: 'italic',
               color: '#fff', 
               textAlign: 'center',
-              lineHeight: 1.6,
-              width: '55%',
-              marginTop: '1rem' 
-            }}>
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
-          eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt."<br/>
-          <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '1rem'}}> ~ Mr. Galloway</cite></p>
+              lineHeight: 2,
+              marginTop: '2rem' 
+            }}> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+             eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt."<br/>
+              <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '3rem'}}> ~ Mr. Galloway</cite></p>
+          </div>
+
+          <div className='blogText' style={{ flex: 1, marginTop: '3rem', width: '50%' }}>
+            <h2 style={{ fontSize: '4rem', color: '#fff', textAlign: 'center' }}> Mindset ipsum dolor sit amet</h2>  
+          </div>
         </div>
-      
+
         <div className='blogContent'
           style={{
             paddingLeft: '1rem',
@@ -166,19 +102,13 @@ const Resource = () => {
           }}>
         
           <Row gutter={20}>
-            {blogPosts.map((post) => (
-              <Col span={8} key={post.id}>
-                <Card 
-                  style={{
-                    backgroundColor: 'pink',
-                    border: 'none', 
-                    marginBottom: '1rem'
-                  }}
-
+            {dietPosts.map((post) => (
+              <Col span={6} key={post.id}>
+                <Card style={{ backgroundColor: 'transparent', border: '1px solid pink', marginBottom: '1rem' }}
                   hoverable
                   cover={ <img src={post.image} alt={post.title} style={{backgroundRepeat: 'no-repeat'}} />} >
   
-                  <Meta title={post.title} description={post.description} />
+                  <Meta title={post.title} description={post.description} style={ {color: 'white'} }/>
                   <LinkRouter to='/newpage'><Button style={{backgroundColor: 'purple', border: 'none', marginTop: '1rem'}}>Read More</Button></LinkRouter>
                 </Card>
               </Col>
@@ -186,8 +116,9 @@ const Resource = () => {
           </Row>
         </div>
       </section>
+      
 
-      {/** Lifestyle*/}
+      {/** Lifestyle Blog Posts*/}
     
       <section id='Lifestyle'>
         <div className='blogHeader'
@@ -200,45 +131,25 @@ const Resource = () => {
             borderRadius: '2rem'
           }}>
         
-          <h2 
-            style={{
-              fontSize: '3rem',
-              color: '#fff',
-              textAlign: 'center',
-            }}> Lifestyle ipsum dolor sit amet</h2>
-        
-          <p 
-            style={{
-              fontSize: '0.9rem',
-              fontStyle: 'italic',
-              color: '#fff', 
-              textAlign: 'center',
-              lineHeight: 1.6,
-              width: '55%',
-              marginTop: '1rem' 
-            }}>
-          "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
+          <h2 style={{ fontSize: '3rem', color: '#fff', textAlign: 'center' }}> Lifestyle ipsum dolor sit amet</h2>
+          <p style={{
+            fontSize: '0.9rem',
+            fontStyle: 'italic',
+            color: '#fff', 
+            textAlign: 'center',
+            lineHeight: 1.6,
+            width: '55%',
+            marginTop: '1rem' 
+          }}> "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam,
           eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt."<br/>
-          <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '1rem'}}> ~ Mr. Galloway</cite></p>
+            <cite style={{ display: 'flex', justifyContent: 'flex-end', fontWeight: 'bold', lineHeight: 2, marginRight: '1rem'}}> ~ Mr. Galloway</cite></p>
         </div>
       
-        <div className='blogContent'
-          style={{
-            paddingLeft: '1rem',
-            paddingRight: '1rem',
-            paddingTop: '1rem'
-          }}>  
-        
+        <div className='blogContent' style={{ paddingLeft: '1rem', paddingRight: '1rem', paddingTop: '1rem' }}>  
           <Row gutter={20}>
-            {blogPosts.map((post) => (
-              <Col span={8} key={post.id}>
-                <Card 
-                  style={{
-                    backgroundColor: 'pink',
-                    border: 'none', 
-                    marginBottom: '1rem'
-                  }}
-
+            {lifestylePosts.map((post) => (
+              <Col span={6} key={post.id}>
+                <Card style={{ backgroundColor: 'pink', border: 'none', marginBottom: '1rem' }}
                   hoverable
                   cover={ <img src={post.image} alt={post.title} style={{backgroundRepeat: 'no-repeat'}} />} >
 
@@ -251,7 +162,7 @@ const Resource = () => {
         </div>
       </section>
 
-      <a href='#' style={{textAlign: 'center', marginTop: '3rem', marginBottom: '2rem'}}>Back to Top</a>
+      <a href='#' style={{ display: 'flex', justifyContent: 'center', alignContent: 'center', marginTop: '3rem', marginBottom: '2rem' }}><FaArrowUp size={30}/> </a>
 
       <div className='socialMedia' style={{color: '#fff', display: 'flex', justifyContent: 'center', alignItems: 'center', marginTop: '2rem'}}>
         Follow us at 
