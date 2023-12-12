40b92c6e810011c4e6522ca21e3101e243ea3fb2
Rediet <redietkfle@gmail.com>
2023-12-12 14:06:45 +0300

    Render Blog page

---
+++ src/Pages/resource.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Resource from '../Components/Resource/Resource';
+import Topbar from '../Components/Resource/Topbar.js';
+
+const ResourcePage = () => {
+    return (
+      <>
+        <Topbar />
+        <Resource />
+      </>
+    );
+}
+
+export default ResourcePage;
