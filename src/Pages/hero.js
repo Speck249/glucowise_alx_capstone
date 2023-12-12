8c3a9526840556fb730a5fd891b5e831681a9192
Rediet <redietkfle@gmail.com>
2023-12-12 14:07:44 +0300

    Update to hero render

--- src/Pages/hero.js
+++ src/Pages/hero.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import Navigation from '../Components/Navigation/Navigation'
 import Hero from '../Components/Hero/Hero';
 import Footer from '../Components/Footer/Footer';
 
 const HeroPage = () => {
     return (
      <>
+       <Navigation />
        <Hero />
        <Footer />
      </>
