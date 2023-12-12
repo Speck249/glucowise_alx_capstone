18f821033760b717a936d90b87b1cc3db562c624
Rediet <redietkfle@gmail.com>
2023-12-12 17:19:50 +0300

    Data source for populating Bootstrap card

---
+++ src/Components/Resource/Data.js
@@ -0,0 +1,101 @@
+import Placeholder from '../../Images/Placeholder.jpg';
+
+export const mindsetPosts = [
+    {
+      id: 'md-1',
+      title: 'Schedule Appointments',
+      description: 'View and Manage Your Upcoming Therapy Sessions.',
+      image: Placeholder,
+    },
+
+    {
+      id: 'md-2',
+      title: 'Medication Tracker',
+      description: 'Track and Manage Your Prescribed Medications.',
+      image: Placeholder,
+    },
+    
+    {
+      id: 'md-3',
+      title: 'Mood Journal',
+      description: 'Record and Monitor Your Daily Mood and Emotions.',
+      image: Placeholder,
+    },
+
+    {
+      id: 'md-4',
+      title: 'Mood Journal',
+      description: 'Record and Monitor Your Daily Mood and Emotions.',
+      image: Placeholder,
+    },
+    {
+      id: 'md-5',
+      title: 'Mood Journal',
+      description: 'Record and Monitor Your Daily Mood and Emotions.',
+      image: Placeholder,
+    },
+    {
+      id: 'md-5',
+      title: 'Mood Journal',
+      description: 'Record and Monitor Your Daily Mood and Emotions.',
+      image: Placeholder,
+    }
+  ];
+  
+export const dietPosts = [
+    {
+      id: 'dp-1',
+      title: 'Therapist Notes',
+      description: 'Access Notes and Feedback From Your Therapist.',
+      image: Placeholder,
+    },        
+    {
+      id: 'dp-2',
+      title: 'Payment Service',
+      description: 'Choose Your Payment Plan and Manage Your Fees all.',
+      image: Placeholder,
+    },    
+    {
+      id: 'dp-3',
+      title: 'Resources Library',
+      description: 'Browse Educational Materials and Helpful Resources.',
+      image: Placeholder,
+    },
+    {
+      id: 'dp-4',
+      title: 'Resources Library',
+      description: 'Browse Educational Materials and Helpful Resources.',
+      image: Placeholder,
+    }
+  ];
+
+
+export const lifestylePosts = [
+    {
+      id: 'lp-1',
+      title: 'Therapist Notes',
+      description: 'Access Notes and Feedback From Your Therapist.',
+      image: Placeholder,
+    },        
+    {
+      id: 'lp-2',
+      title: 'Payment Service',
+      description: 'Choose Your Payment Plan and Manage Your Fees all.',
+      image: Placeholder,
+    }, 
+    {
+      id: 'lp-3',
+      title: 'Resources Library',
+      description: 'Browse Educational Materials and Helpful Resources.',
+      image: Placeholder,
+    },
+    {
+      id: 'lp-3',
+      title: 'Resources Library',
+      description: 'Browse Educational Materials and Helpful Resources.',
+      image: Placeholder,
+    }
+  ];
+
+
+  
