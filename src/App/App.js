99365b5e363bc63f094b1effce04ae67486d3c4c
Rediet <redietkfle@gmail.com>
2023-12-16 22:07:10 +0300

    Update website routing

--- src/App/App.js
+++ src/App/App.js
@@ -5,26 +5,37 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import HeroPage from '../Pages/hero.js';
-import DashboardPage from '../Pages/dashboard.js';
 import ResourcePage from '../Pages/resource.js';
 import SupportPage from '../Pages/support.js';
 import LoginPage from '../Pages/login.js';
 import SignupPage from '../Pages/signup.js';
+import ProtectedRoute from '../Components/Authentication/ProtectedRoute.js';
+import DashboardPage from '../Pages/dashboard.js';
+import { UserAuthContextProvider } from '../Context/userAuthContext.js';
 import './App.css';
 
-const App = () => {
-    return (
-        <Router>
-          <Routes>
-            <Route path='/' element={<HeroPage />} />
-            <Route path='/dashboard' element={<DashboardPage />} />
-            <Route path='/resource' element={<ResourcePage />} /> 
-            <Route path='/support' element={<SupportPage />} /> 
-            <Route path='/login' element={<LoginPage />} />
-            <Route path='/signup' element={<SignupPage />} />
-          </Routes>
-        </Router>
-    )
+function App() {
+  return (
+    <Router>
+      <UserAuthContextProvider>
+        <Routes>
+          <Route path='/' element={<HeroPage />} />
+          <Route path='/resource' element={<ResourcePage />} /> 
+          <Route path='/support' element={<SupportPage />} /> 
+          <Route path='/login' element={<LoginPage />} />
+          <Route path='/signup' element={<SignupPage />} />
+           <Route
+            path='/dashboard'
+            element={
+              <ProtectedRoute>
+                <DashboardPage />
+              </ProtectedRoute>
+            }
+          />
+        </Routes>
+      </UserAuthContextProvider>
+    </Router>
+  );
 }
 
 export default App;
