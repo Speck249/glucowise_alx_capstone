f1709673911fac8fd2e62706c9fda7e5463044a4
Rediet <redietkfle@gmail.com>
2023-12-17 08:56:43 +0300

    Update page routing logic

--- src/App/App.js
+++ src/App/App.js
@@ -2,36 +2,36 @@
 // organizing, managing component relationships and setting up routing mechanisms.
 // It defines the overall structure and behavior of the React application.  
 
-import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HeroPage from '../Pages/hero.js';
-import ResourcePage from '../Pages/resource.js';
-import SupportPage from '../Pages/support.js';
-import LoginPage from '../Pages/login.js';
-import SignupPage from '../Pages/signup.js';
-import ProtectedRoute from '../Components/Authentication/ProtectedRoute.js';
-import DashboardPage from '../Pages/dashboard.js';
 import { UserAuthContextProvider } from '../Context/userAuthContext.js';
+import HeroPage from '../Pages/hero.js';
+import DashboardPage from '../Pages/dashboard.js';
+import SupportPage from '../Pages/support.js';
+import ResourcePage from '../Pages/resource.js';
+import SignupPage from '../Pages/signup.js';
+import LoginPage from '../Pages/login.js';
+import ProtectedRoute from '../Components/Authentication/ProtectedRoute.js';
 import './App.css';
 
-function App() {
+
+const App = () => {
   return (
     <Router>
       <UserAuthContextProvider>
         <Routes>
-          <Route path='/' element={<HeroPage />} />
-          <Route path='/resource' element={<ResourcePage />} /> 
-          <Route path='/support' element={<SupportPage />} /> 
-          <Route path='/login' element={<LoginPage />} />
-          <Route path='/signup' element={<SignupPage />} />
-           <Route
+          <Route
             path='/dashboard'
             element={
               <ProtectedRoute>
-                <DashboardPage />
+                <DashboardPage/>
               </ProtectedRoute>
             }
           />
+          <Route path='/' element={<HeroPage />} />
+          <Route path='/support' element={<SupportPage />} />
+          <Route path='/resource' element={<ResourcePage />} />
+          <Route path='/signup' element={<SignupPage />} />
+          <Route path='/login' element={<LoginPage />} />
         </Routes>
       </UserAuthContextProvider>
     </Router>
