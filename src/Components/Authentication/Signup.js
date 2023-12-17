0063cf8f2658cd2ab8f46b20bf522341ae413ca7
Rediet <redietkfle@gmail.com>
2023-12-17 14:26:51 +0300

    Update component layout

--- src/Components/Authentication/Signup.js
+++ src/Components/Authentication/Signup.js
@@ -1,17 +1,78 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { useUserAuth } from '../../Context/userAuthContext';
+import { useNavigate } from 'react-router-dom';
+import { Link as LinkRouter } from 'react-router-dom';
+import Alert from '@mui/material/Alert';
+import Stack from '@mui/material/Stack';
 import Google from '../../Images/googleIcon.png';
 import './Signup.css'
 
 
 const Signup = () => {
+  // Firebase authentication configuration for user Registration.
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [infoMessage, setInfoMessage] = useState('');
+  const { signUp, googleSignUp } = useUserAuth();
+  const navigate = useNavigate();
+
+  const handleSignup = async (e) => {
+    e.preventDefault();
+    setError(null);
+    setInfoMessage('');
+
+ // Send verification email for registration then redirect user
+ // home page once successfully logged in or terminate registration
+ // attempt with error message.
+    try {
+      await signUp(email, password);
+      setInfoMessage('Verification Email Sent');
+    } catch (error) {
+      setError(error.message);
+      setInfoMessage('');
+    }
+  };
+
+// Clear out email and password field in the event that user
+// user logs in or fails to login.
+  useEffect(() => {
+    const clearFields = setTimeout(() => {
+      setEmail('');
+      setPassword('');
+      setError(null);
+      setInfoMessage('');
+    }, 5000);
+
+    return () => {
+      clearTimeout(clearFields);
+    };
+  }, [email, password, error, infoMessage]);    
+
+// Handle alternative Google registration option, which then
+// redirects user to Home page once logged in or terminate attempt
+// with an error message.
+  const handleGoogleSignUp = async (e) => {
+    try {
+      await googleSignUp();
+      navigate('/home');
+    } catch (error) {
+      console.log(error.message);
+    }
+  };
+  
+  // Return component output to Home page for rendering.
   return (
     <div className='signupContainer'>
       <div className='formWrap'>
         <div className='formContent'>
-          <div className='form'> 
-            <h1>SIGN UP</h1>
-            <button className='googleContainer'><span className='imgContainer'>
+          <form id='signupForm' onSubmit={handleSignup}> 
+            <Stack spacing={1}>
+              {error && <Alert severity='error'>{error}</Alert>}
+              {infoMessage && <Alert severity='success'>{infoMessage}</Alert>}
+            </Stack>
+            <h1 style={{marginTop: '0.5rem'}}>Sign Up</h1>
+            <button className='googleContainer' onClick={handleGoogleSignUp}><span className='imgContainer'>
               <img 
                 src={Google}
                 width={25}
@@ -24,71 +85,41 @@ const Signup = () => {
               <div className='dividerContent'>or</div>
             </div>
 
-            <div className='nameLabel'>
-              <label htmlFor='firstName'>First Name</label>
-              <label htmlFor='lastName'>Last Name</label>
-            </div>
-
-            <div className='nameInput'>  
-              <input
-                type='text'
-                id='firstName'
-                name='firstName'
-              //value={firstName}
-              //onChange={(e) => setfirstName(e.target.value)} 
-                placeholder='Aster'
-                autoComplete='on'
-                autoFocus
-                required
-              />
-       
-              <input
-                type='text'
-                id='lastName'
-                name='lastName'
-              //value={lastName}
-              //onChange={(e) => setlastName(e.target.value)} 
-                placeholder='Awoke' 
-                autoComplete='on'
-                required
-              />
-            </div>
-
-            <label htmlFor='email'>Email</label>
-            <input
-              type='email'
-              id='email-input'
-              name='email-input'
-            //value={email}
-            //onChange={(e) => setEmail(e.target.value)}
-              placeholder='asterawoke@gmail.com'
-              autoComplete='on'
-              required
-            />
+          <label htmlFor='email'>Email</label>
+          <input
+            type='email'
+            id='email-input'
+            name='email-input'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder='asterawoke@gmail.com'
+            autoComplete='on'
+            required
+          />
             
-            <label htmlFor='password'>Password</label>
-            <input
-              type='password'
-              id='pwd-input'
-              name='pwd-input'
-            //value={password}
-            //onChange={(e) => setPassword(e.target.value)}
-              placeholder='Create a strong password'
-              autoComplete='on'
-              required
-            />
+          <label htmlFor='password'>Password</label>
+          <input
+            type='password'
+            id='pwd-input'
+            name='pwd-input'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder='Create a strong password'
+            autoComplete='on'
+            required
+          />
             
-            <div className='buttonContainer'>
-              <button type='submit'>Sign Up</button>
-            </div>
-
-            <div className='prompt'> 
-              <p>Already have an account? <span><Link to='/login'>Log In</Link></span></p>
-            </div> 
+          <div className='buttonContainer'>
+            <button type='submit'>Sign Up</button>
           </div>
-        </div>
+
+          <div className='prompt'> 
+            <p>Already have an account? <span><LinkRouter style={{ textDecoration: 'none' }} to='/'>Log In</LinkRouter></span></p>
+          </div> 
+        </form>
       </div>
     </div>
+  </div>
   );
 };
 
