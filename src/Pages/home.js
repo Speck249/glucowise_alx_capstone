0fc009e52ca4e2ece94e09a21e6ec9d251a26c63
Rediet <redietkfle@gmail.com>
2023-12-17 14:32:10 +0300

    Update home routing logic

---
+++ src/Pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Navigation from '../Components/Navigation/Navigation'
+import Hero from '../Components/Hero/Hero';
+import Footer from '../Components/Footer/Footer';
+
+const HomePage = () => {
+    return (
+     <>
+       <Navigation />
+       <Hero />
+       <Footer />
+     </>
+  );
+}
+
+export default HomePage;
