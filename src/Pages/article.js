fe6dfde757810e2bccf8572d548163d2f7ad6034
Rediet <redietkfle@gmail.com>
2023-12-20 14:48:47 +0300

    Render blog page

---
+++ src/Pages/article.js
@@ -0,0 +1,11 @@
+import React from "react";
+import Article from "../Components/Resource/Article";
+
+
+const ArticlePage = () => {
+    <>
+      <Article />
+    </>
+}
+
+export default ArticlePage;
