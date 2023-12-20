d2d60970df453ef5215e612b06237671df71ec8f
Rediet <redietkfle@gmail.com>
2023-12-20 16:56:56 +0300

    Insert optimized card images

--- src/Components/Resource/Data.js
+++ src/Components/Resource/Data.js
@@ -1,44 +1,58 @@
-import Placeholder from '../../Images/Placeholder.jpg';
+import Growth from '../../Images/Growth.jpg';
+import Stress from '../../Images/Stress.jpg';
+import Positivity from '../../Images/Positivity.jpg';
+import Highachievers from '../../Images/Highachievers.jpg';
+import Optimism from '../../Images/Optimism.jpg';
+import Master from '../../Images/Master.jpg';
+import Paleo from '../../Images/Paleo.jpg';
+import Diet from '../../Images/Diet.jpg';
+import Medicine from '../../Images/Medicine.jpg';
+import Bowl from '../../Images/Bowl.jpg';
+import Balance from '../../Images/Balance.jpg';
+import Now from '../../Images/Now.jpg';
+import Meditation from '../../Images/Meditation.jpg';
+import Intentional from '../../Images/Intentional.jpg';
+
 
 export const mindsetPosts = [
     {
       id: 'md-1',
       title: 'Developing a Growth Mindset',
       description: 'Discover the transforming power of a Growth mindset.',
-      image: Placeholder,
+      image: Growth,
     },
 
     {
       id: 'md-2',
       title: 'Stress Management',
       description: 'Explore the transformative effects of a resilient mindset.',
-      image: Placeholder,
+      image: Stress,
     },
     
     {
       id: 'md-3',
-      title: 'The Power of Optimisim',
+      title: 'The Power of Positivity',
       description: 'The power of a positive mindset to unleash a potential.',
-      image: Placeholder,
+      image: Positivity,
     },
 
     {
       id: 'md-4',
       title: 'Secrets of High Achievers',
       description: 'Learn how to adopt a winning attitude to reach new heights.',
-      image: Placeholder,
+      image: Highachievers,
     },
     {
       id: 'md-5',
       title: 'Cultivate Optimism',
       description: 'Self-belief and resilience for a happier, more fulfilling journey.',
-      image: Placeholder,
+      image: Optimism,
     },
     {
       id: 'md-5',
       title: 'Master your mind, master your life',
       description: 'Explore changes that can empower you to break remove limitations.',
-      image: Placeholder,
+      image: Master,
     }
   ];
   
@@ -47,25 +61,25 @@ export const dietPosts = [
       id: 'dp-1',
       title: 'Discover Paleo Diet',
       description: 'Discover the keys to a healthy and nourshing diet.',
-      image: Placeholder,
+      image: Paleo,
     },        
     {
       id: 'dp-2',
       title: 'Redefining Healhy Diet',
       description: 'Delicious recipes, and the science of nutritious choices.',
-      image: Placeholder,
+      image: Diet,
     },    
     {
       id: 'dp-3',
       title: 'Food as Medicine',
       description: 'Promoting longevity, and enhancing your overall vitality.',
-      image: Placeholder,
+      image: Medicine,
     },
     {
       id: 'dp-4',
       title: 'Vitality in a Bowl',
       description: 'Learn the crucial role of balanced diet in preventing illness.',
-      image: Placeholder,
+      image: Bowl,
     }
   ];
 
@@ -75,25 +89,25 @@ export const lifestylePosts = [
       id: 'lp-1',
       title: 'The myth of work-life balance',
       description: 'Design a lifestyle of your greatest aspirations.',
-      image: Placeholder,
+      image: Balance,
     },        
     {
       id: 'lp-2',
       title: 'Living in the Now',
       description: 'Cultivate self-care practices, and the of joy moments.',
-      image: Placeholder,
+      image: Now,
     }, 
     {
       id: 'lp-3',
       title: 'The Power of Meditation',
       description: 'Embrace a mindful lifestyle for your mind, body, and soul.',
-      image: Placeholder,
+      image: Meditation,
     },
     {
       id: 'lp-3',
       title: 'Intentional Living',
       description: 'Uncover the transformative benefits of mindfulness.',
-      image: Placeholder,
+      image: Intentional,
     }
   ];
 
