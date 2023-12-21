2dcba25398ff11ce77b8ba5f0ff556d2639bf570
Rediet <redietkfle@gmail.com>
2023-12-21 08:57:06 +0300

    Update sidebar logic and styling

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,36 +1,39 @@
 import React from 'react';
-import { FaTimes } from 'react-icons/fa';
 import { Link as LinkRouter } from 'react-router-dom';
 import { Link as LinkScroll } from 'react-scroll';
+import Scrollspy from 'react-scrollspy';
+import { FaTimes } from 'react-icons/fa';
 import './Sidebar.css';
 
-
-const Sidebar = () => {
+const Sidebar = ({ isOpen, toggle }) => {
   return (
-    <>
-      <div className='sidebarContainer'>
-        <div className='Icon'>
-          <div className='closeIcon'>
-            <FaTimes />
-          </div>
+    <div className='sidebarContainer' isOpen={isOpen} onClick={toggle}>
+      <div className='Icon' onClick={toggle}>
+        <div className='closeIcon'>
+          <FaTimes />
         </div>
+      </div>
        
+      <Scrollspy
+        items={ ['About', 'Usage', 'Insurance']}
+        currentActiveClass='active'
+        offset={-100}
+      >
         <div className='sidebarWrapper'>
           <div className='sidebarMenu'>
-            <LinkScroll to='section1' smooth={true} duration={500} offset={-50}>About</LinkScroll>
-            <LinkScroll to='section2' smooth={true} duration={500} offset={-50}>What It Does</LinkScroll>
-            <LinkScroll to='section3' smooth={true} duration={500} offset={-50}>How It Works</LinkScroll>
-            <LinkRouter to='/dashboard'>Dashboard</LinkRouter>
-            <LinkRouter to='/blog'>Blog</LinkRouter>
-            <LinkRouter to='/contact'>Contact</LinkRouter>
+            <LinkScroll to='About' activeClass='active' spy={true} smooth={true} duration={500} offset={-50} onClick={toggle}> About GlucoWise </LinkScroll>
+            <LinkScroll to='Usage' activeClass='active' spy={true} smooth={true} duration={500} offset={-50} onClick={toggle}> How It Works </LinkScroll>
+            <LinkScroll to='Insurance' activeClass='active' spy={true} smooth={true} duration={500} offset={-50} onClick={toggle}> Cost & Coverage </LinkScroll>
+            <LinkRouter style={{ color: 'bisque', textDecoration: 'none'}} to='/support' onClick={toggle}> Support </LinkRouter>
+            <LinkRouter style={{ color: 'bisque', textDecoration: 'none'}} to='/resource' onClick={toggle}> Resource </LinkRouter>
           </div>
-       </div>
+        </div>
    
-       <div className='buttonWrap'>
-         <LinkRouter to='/login'>Get Started</LinkRouter>
-       </div>
-      </div>
-    </>
+        <div className='buttonWrap'>
+          <LinkRouter style={{ color: 'bisque', textDecoration: 'none'}} to='/dashboard'>Get Started</LinkRouter>
+        </div>
+      </Scrollspy>
+    </div>
   ); 
 }
 
