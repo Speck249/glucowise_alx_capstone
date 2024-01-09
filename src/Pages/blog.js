6433691edd5f6ff4bbfa199158682ec61092c32d
Rediet <redietkfle@gmail.com>
2024-01-09 15:20:36 +0300

    Renamed Blog page

---
+++ src/Pages/blog.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Blog from '../Components/Resource/Blog';
+
+
+const BlogPage = () => {
+  return(
+    <>
+      <Blog />
+    </>
+  )
+}
+
+export default BlogPage;
